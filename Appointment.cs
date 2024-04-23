79e1c53b02137cefd063cd9e3384883ca71ee882
Shayaan <Shayaan.rasool@marquette.edu>
2024-04-23 14:59:22 -0500

    Create Appointment class

---
+++ Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System.Dynamic;
+
+namespace ClassProject001;
+
+public class Appointment
+{
+
+    public int id { get; set; }
+    public DateTime dateTime { get; set; }
+    public string Description { get; set; }
+
+}
