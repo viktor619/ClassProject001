5a7fa4ac8b4a62aebc15d73e7a766af17c6ebfb4
Gerardo Perez <gerardo.perez@marquette.edu>
2024-05-02 16:07:25 -0500

    customer reservation

---
+++ CustomerReservation001.cs
@@ -0,0 +1,16 @@
+﻿using ClassProject001;
+
+namespace ClassProject003;
+
+public class CustomerReservation001
+{
+    public Customer c {get; set;}
+    public Reservation r1 {get; set;}
+
+    public CustomerReservation001(Customer c, Reservation r1 )
+    {
+        this.c = c;
+        this.r1 = r1;
+    }
+
+}
