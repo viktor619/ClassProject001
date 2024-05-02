aed8d553f89aa70c97f023de4acf1fc0da944792
Brayan <brayan.velazquez@marquette.edu>
2024-05-02 16:07:06 -0500

    Added Customer Class

---
+++ Customer001.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassProject001;
+
+public class Customer001
+{
+     private static int autoIncrement;
+    public int Id {get;}
+    public int PhoneNumber{get; set;}
+    public string Username {get; set;}
+    public string Password {get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    
+
+    public Customer001()
+    {
+        autoIncrement++;
+        Id = autoIncrement;
+    }
+}
